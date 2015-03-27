b2842d08cf8d23c50e9e0be9af026f14f320bfac
Merouane Atig <merouane.atig@gmail.com>
2015-03-27 21:58:48 +0100

    Create new cookbook using berkshelf

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 0.1.0
+
+Initial release of nmap-cookbook
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'nmap-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures nmap-cookbook'
+long_description 'Installs/Configures nmap-cookbook'
+version          '0.1.0'
